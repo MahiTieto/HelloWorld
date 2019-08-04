5bc4c12c63d3417c3ada7062d207c42ea115c5fd
MahiTieto <53614657+MahiTieto@users.noreply.github.com>
2019-08-04 13:43:43 +0530

    java changes

---
+++ test.java
@@ -0,0 +1,15 @@
+import  java.util.*;
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("test again");
+        int a,b,c;
+
+        a=10;
+        b=5;
+
+        c=a+b;
+
+        System.out.println("value of c is "+c);
+    }
+}
