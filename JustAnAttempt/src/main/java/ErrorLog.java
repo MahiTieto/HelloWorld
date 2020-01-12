600abfa54752fa4c775035e3bcd431973034da7e
Maheedhar Reddy <luckymahi.b@gmail.com>
2020-01-12 12:54:53 +0530

    Temp 12/01

--- JustAnAttempt/src/main/java/ErrorLog.java
+++ JustAnAttempt/src/main/java/ErrorLog.java
@@ -1,12 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
+import javax.swing.ButtonGroup;
 import javax.swing.ListSelectionModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -14,11 +14,13 @@ import javax.swing.border.CompoundBorder;
 import com.toedter.calendar.JDateChooser;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 
 public class ErrorLog extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -39,6 +41,8 @@ public class ErrorLog extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+
 	public ErrorLog() {
 		setTitle("Error Log");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,34 +52,56 @@ public class ErrorLog extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(39, 45, 1, 1);
-		contentPane.add(list);
-		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBorder(new CompoundBorder());
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"BIC", "CAMS", "AUTOGIRO", "TILLBIC", "UP", "FASIT"}));
 		comboBox.setToolTipText("Select an Application");
-		comboBox.setBounds(12, 13, 136, 22);
+		comboBox.setBounds(186, 13, 136, 22);
 		contentPane.add(comboBox);
 		
 		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(182, 65, 118, 22);
+		dateChooser.setBounds(186, 65, 116, 22);
 		contentPane.add(dateChooser);
 		
 		JLabel lblNewLabel = new JLabel("Date");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
 		lblNewLabel.setBounds(12, 65, 87, 22);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Job");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 22));
-		lblNewLabel_1.setBounds(12, 124, 103, 22);
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_1.setBounds(12, 117, 103, 22);
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(184, 124, 116, 22);
+		textField.setBounds(186, 117, 116, 22);
 		contentPane.add(textField);
 		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Program");
+		lblNewLabel_2.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_2.setBounds(12, 169, 118, 32);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(186, 169, 116, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Select App");
+		lblNewLabel_3.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_3.setBounds(12, 13, 129, 30);
+		contentPane.add(lblNewLabel_3);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Insert");
+		rdbtnNewRadioButton.setBounds(686, 12, 127, 25);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Search");
+		rdbtnNewRadioButton_1.setBounds(686, 65, 127, 25);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+
 	}
+		
 }
