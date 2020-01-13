95b259c8c29a409df9a20c276753b08b0fe19b61
Maheedhar Reddy <luckymahi.b@gmail.com>
2020-01-13 16:06:07 +0530

    Form Complete text limitation pending

--- JustAnAttempt/src/main/java/ErrorLog.java
+++ JustAnAttempt/src/main/java/ErrorLog.java
@@ -15,12 +15,19 @@ import com.toedter.calendar.JDateChooser;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import java.awt.Toolkit;
+import javax.swing.border.TitledBorder;
 
 public class ErrorLog extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
 
 	/**
 	 * Launch the application.
@@ -48,7 +55,7 @@ public class ErrorLog extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(300, 100, 850, 600);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
@@ -92,14 +99,75 @@ public class ErrorLog extends JFrame {
 		lblNewLabel_3.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
 		lblNewLabel_3.setBounds(12, 13, 129, 30);
 		contentPane.add(lblNewLabel_3);
+	
+		ButtonGroup buttonGroup = new ButtonGroup();
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Insert");
-		rdbtnNewRadioButton.setBounds(686, 12, 127, 25);
-		contentPane.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Search");
-		rdbtnNewRadioButton_1.setBounds(686, 65, 127, 25);
-		contentPane.add(rdbtnNewRadioButton_1);
+		JRadioButton Insert = new JRadioButton("Insert");
+		Insert.setBounds(686, 12, 127, 25);
+		contentPane.add(Insert);
+        JRadioButton Search = new JRadioButton("Search");
+        Search.setBounds(686, 65, 127, 25);
+        contentPane.add(Search);
+        
+		buttonGroup.add(Insert);
+        buttonGroup.add(Search);
+ 
+        getContentPane().add(Insert);
+        getContentPane().add(Search);
+ 
+        Insert.setSelected(true);
+        
+        JLabel lblNewLabel_4 = new JLabel("Error Code");
+        lblNewLabel_4.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+        lblNewLabel_4.setBounds(12, 221, 129, 32);
+        contentPane.add(lblNewLabel_4);
+        
+        textField_2 = new JTextField();
+        textField_2.setBounds(186, 221, 116, 22);
+        contentPane.add(textField_2);
+        textField_2.setColumns(10);
+        
+        JLabel lblNewLabel_5 = new JLabel("INIT");
+        lblNewLabel_5.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+        lblNewLabel_5.setBounds(12, 273, 118, 32);
+        contentPane.add(lblNewLabel_5);
+        
+        textField_3 = new JTextField();
+        textField_3.setBounds(186, 273, 116, 22);
+        contentPane.add(textField_3);
+        textField_3.setColumns(10);
+        
+        JLabel lblNewLabel_6 = new JLabel("ARS");
+        lblNewLabel_6.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+        lblNewLabel_6.setBounds(12, 325, 87, 32);
+        contentPane.add(lblNewLabel_6);
+        
+        textField_4 = new JTextField();
+        textField_4.setBounds(186, 325, 116, 22);
+        contentPane.add(textField_4);
+        textField_4.setColumns(10);
+        
+        JLabel lblNewLabel_7 = new JLabel("Solution");
+        lblNewLabel_7.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+        lblNewLabel_7.setBounds(12, 377, 103, 32);
+        contentPane.add(lblNewLabel_7);
+        
+        textField_5 = new JTextField();
+        textField_5.setBounds(186, 377, 627, 163);
+        contentPane.add(textField_5);
+        textField_5.setColumns(10);
+        
+        JLabel lblNewLabel_8 = new JLabel("Problem Desc.");
+        lblNewLabel_8.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 22));
+        lblNewLabel_8.setBounds(354, 169, 166, 32);
+        contentPane.add(lblNewLabel_8);
+        
+        textField_6 = new JTextField();
+        textField_6.setBounds(354, 221, 459, 131);
+        contentPane.add(textField_6);
+        textField_6.setColumns(10);
+        
+        
 		
 
 	}
